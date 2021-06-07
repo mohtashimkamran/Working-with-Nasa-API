2876575fe55350f02b52f8bb1a17d32dae57eccd
mohtashimkamran <mk.mohtashim@gmail.com>
2021-06-07 20:13:46 +0530

    first commit

---
+++ Mars Rover/main.js
@@ -0,0 +1,73 @@
+var sol = $('#sol');
+var pageno = 0;
+var prev = $('#prev');
+var next = $('#next');
+
+
+function displayImage(data){
+    console.log(data.photos[0].img_src);
+    for (var i =0 ; i<data.photos.length;i++){
+        // console.log(data.photos[i].img_src);
+        image_url = data.photos[i].img_src;
+        $('<img>').attr({
+            src:data.photos[i].img_src,
+        }).appendTo('#image-container');
+    };
+};
+
+
+$('#btn').click(function(){
+    $('#image-container').remove('img');
+    if (sol.val()===""){
+        alert("Feilds are mandatory!");
+    }
+    else{
+    $.ajax({
+        url:'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+        method : 'GET',
+        success : displayImage,
+        data : {
+            api_key : "UnsTs4Kq8fdJUO3goioG22Ua0EcOIimZSmx7YBqg",
+            sol:sol.val(),
+            page:pageno,
+        },
+    });    
+};
+});
+
+next.click(function(){
+    pageno++;
+    if (sol.val()===""){
+        alert("Feilds are mandatory!");
+    }
+    else{
+    $.ajax({
+        url:'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+        method : 'GET',
+        success : displayImage,
+        data : {
+            api_key : "UnsTs4Kq8fdJUO3goioG22Ua0EcOIimZSmx7YBqg",
+            sol:sol.val(),
+            page:pageno,
+        },
+    });    
+};
+});
+prev.click(function(){
+    pageno--;
+    if (sol.val()===""){
+        alert("Feilds are mandatory!");
+    }
+    else{
+    $.ajax({
+        url:'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+        method : 'GET',
+        success : displayImage,
+        data : {
+            api_key : "UnsTs4Kq8fdJUO3goioG22Ua0EcOIimZSmx7YBqg",
+            sol:sol.val(),
+            page:pageno,
+        },
+    });    
+};
+});
---
+++ main.js
@@ -0,0 +1,30 @@
+var currdate = $('#date');
+
+
+function displayImage(data){
+    // console.log(data)
+    for (var i =0;i<data.length;i++){
+        console.log(data[i]);
+        $('<img>',{
+        src:data[i].url,
+    }).appendTo('#image-container');
+    };
+};
+$('#btn').click(function(){
+    if(currdate.val() === ""){
+        alert("Enter a date!")
+    }
+    else {
+    $.ajax({
+        url:"https://api.nasa.gov/planetary/apod",
+        method:'GET',
+        success : displayImage,
+        data : {
+            api_key: 'UnsTs4Kq8fdJUO3goioG22Ua0EcOIimZSmx7YBqg',
+            count:'12',
+            thumbs:true,
+        }
+    })};
+    // currdate.hide();
+    // $('#btn').hide();
+});
